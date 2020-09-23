6947dc57a8cc5cb6938e6bbc7e54883d552d36f5
Anirudh Shenoy <anirudhshenoy@Anirudhs-MacBook-Air.local>
2020-09-23 11:35:40 +0530

    initial commit

---
+++ docs/allFunctions.md
@@ -0,0 +1,31 @@
+---
+id: allFunctions
+title: allFunctions
+sidebar_label: app.allFunctions
+---
+
+Created by Harish Mukkara
+
+### Input Parameters
+
+
+### Output Parameters
+
+
+This will list all the functions built for bot
+
+### Examples
+
+```javascript
+let allFunctions = app.log(Object.keys(app.allFunctions));
+
+// the log will be
+[
+ main,
+ didYouMean,
+ .
+ .
+ allcloudfuctions
+ .
+]
+```
---
+++ sidebars.js
@@ -0,0 +1,31 @@
+module.exports = {
+  // someSidebar: {
+  //   Docusaurus: ['doc1', 'doc2', 'doc3'],
+  //   methods: ['allFunctions'],
+  //   Features: ['mdx'],
+  // },
+  docs :{
+  'API Documentation': [
+    {Objects : ['allFunctions', 'app.ai']},
+    {Methods: ['doc1']}
+    ],
+  'Release Notes' : [
+    'Aug 2020',
+    'Sep 2020'
+  ]
+  }
+  // docs: {
+  //   Guides: [
+  //     'doc1',
+  //     {
+  //       Docs: ['doc2'],
+  //     },
+  //   ],
+  //   'Guides 2': [
+  //     'doc3', 
+  //     {
+  //       Methods: ['mdx']
+  //     },
+  //   ]
+  // },
+};
